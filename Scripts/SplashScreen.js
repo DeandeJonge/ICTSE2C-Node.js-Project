5ed5a66a6c3b7fce2b50ad0a003770386d544060
wesley2510 <wesleyoosterveen94@gmail.com>
2016-04-03 20:04:43 +0200

    Added splash screen button and redirect

--- Scripts/SplashScreen.js
+++ Scripts/SplashScreen.js
@@ -1,6 +1,15 @@
 $(window).load(function () {
     $('#splashscreen').fadeOut(5000);
-});
-/**
- * Created by Wes on 3-4-2016.
- */
+    $('#startGame').hide().delay(5000).fadeIn(1000);
+    $("#startGame").click(function () {
+        pageRedirect('index');
+    })
+})
+
+function pageRedirect($page) {
+    console.log('test1');
+    if ($page === 'index') {
+        console.log('test2');
+        window.location.replace('./Index.html');
+    }
+}
