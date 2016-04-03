0f12a78e2fffc7577fce279095b4b4b051d2a77b
wesley2510 <wesleyoosterveen94@gmail.com>
2016-04-03 20:31:04 +0200

    11

--- Scripts/SplashScreen.js
+++ Scripts/SplashScreen.js
@@ -4,12 +4,11 @@ $(window).load(function () {
     $("#startGame").click(function () {
         pageRedirect('index');
     })
+
 })
 
 function pageRedirect($page) {
-    console.log('test1');
     if ($page === 'index') {
-        console.log('test2');
         window.location.replace('./Index.html');
     }
 }
